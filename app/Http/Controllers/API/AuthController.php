d38bbfa671ba648b1e340cb0051faf97f50ca3f4
DRajaNama <rajanamdav@gmail.com>
2019-12-21 19:00:24 +0530

    Update with Api

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; 
+use App\User; 
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+
+    public function register(Request $request) {    
+        $validator = Validator::make($request->all(), 
+                     [ 
+                     'name' => 'required',
+                     'email' => 'required|email',
+                     'password' => 'required',  
+                     'c_password' => 'required|same:password', 
+                    ]);   
+        if ($validator->fails()) {          
+              return response()->json(['error'=>$validator->errors()], 401); 
+        }    
+        $input = $request->all();  
+        $input['password'] = bcrypt($input['password']);
+        $input['api_token'] = Str::random(60);
+        $user = User::create($input); 
+        $data['token'] =  $user->createToken('AppName')->accessToken;
+        return response()->json(['success'=>true,'data'=>$data], $this->successStatus); 
+    }
+
+
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $user->api_token = $user->createToken('AppName')-> accessToken;
+            $user->save(); 
+            return response()->json(['success'=>true,'data'=>$user], $this-> successStatus); 
+        } else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
+    }
+        
+    public function getUser() {
+        $data = Auth::user();
+        return response()->json(['success'=>true,'data'=>$data], $this->successStatus); 
+    }
+        
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,22 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('v1')->group(function(){
+
+
+
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::post('getUser', 'Api\AuthController@getUser');
+    });
+
+
 });
+   
